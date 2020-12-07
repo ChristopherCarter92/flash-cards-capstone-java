6964043e59783110a1d7081e473cf1efb56126a1
Megan Janes <mjanes71@gmail.com>
2020-12-07 12:12:12 -0500

    added CardList view to homepage but was not able to render data on page because store cards array is never populated

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
     <h1 id="home-title">Home</h1>
-    <p id="placeholder">Placeholder for Cards List</p>
-    <router-view></router-view>
+    <cards-list id="cards-list-in-home"></cards-list>
   </div>
 </template>
 
 <script>
+import cardsList from '@/views/CardsList.vue';
 export default {
-  
+
+  components: {
+    cardsList
+  }
 };
 </script>
 <style scoped>
@@ -29,7 +32,7 @@ export default {
   justify-self: center;
 }
 
-#placeholder {
+#cards-list-in-home {
   grid-area: Card-List;
   justify-self: center;
 }
