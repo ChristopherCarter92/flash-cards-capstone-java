4765499fb69b3992ea708af873d4a8e54750f00c
Megan Janes <mjanes71@gmail.com>
2020-12-07 11:53:11 -0500

    adding grids and flexboxes to card details

--- frontend/src/components/CardDetails.vue
+++ frontend/src/components/CardDetails.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <div v-show="faceUp">
+  <div id="card-details-container">
+    <div class="card-face" v-show="faceUp">
       <h2>{{ thisCard.question }}</h2>
-      <button v-on:click.prevent="flipCard">Show me the answer</button>
-      <footer>{{ thisCard.tags }}</footer>
+      <b-button class="flip-button" v-on:click.prevent="flipCard">Show me the answer</b-button>
+      <footer>{{ `Tag(s): ${thisCard.tags}` }}</footer>
     </div>
-    <div v-show="!faceUp">
+    <div class="card-face" v-show="!faceUp">
       <h2>{{ thisCard.answer }}</h2>
-      <button v-on:click.prevent="flipCard">Show Question</button>
+      <b-button class="flip-button" v-on:click.prevent="flipCard">Show Question</b-button>
     </div>
   </div>
 </template>
@@ -53,5 +53,37 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+* {
+  box-sizing: border-box;
+}
+
+h2 {
+  padding: 10vh;
+  font-size: 5em;
+}
+
+
+.card-face {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10% 0 0 0;
+  border-radius: 5px;
+  border: solid  #95B0B6 5px;
+  height: 70vh;
+  width: 70vh;
+  justify-content: center;
+}
+
+.flip-button {
+  width: 70%; 
+  margin: 1em 0 1em 0;
+  background-color: #569FAD;
+  border: 1px solid transparent;
+  padding: 7px;
+  color: #324B50;
+}
+
 </style>
--- frontend/src/views/Card.vue
+++ frontend/src/views/Card.vue
@@ -18,6 +18,8 @@ export default {
 <style scoped>
 * {
   box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 #card-container-card-view {
@@ -25,14 +27,14 @@ export default {
   grid-template-columns: 1fr;
   grid-template-areas: 
   "card";
+  height: 100vh;
 }
 
 #card-details-card-view {
   grid-area: card;
   justify-self: center;
+  height: 50vh;
+  width: auto;
 }
-
-
-
 
 </style>
