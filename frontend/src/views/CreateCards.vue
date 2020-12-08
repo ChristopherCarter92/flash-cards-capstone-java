3d6d4ae9f67ba7feb8917995115582c326bf1d31
Megan Janes <mjanes71@gmail.com>
2020-12-08 12:56:04 -0500

    added CreateCards.vue

---
+++ frontend/src/views/CreateCards.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>Create New Card</h1>
+    <add-card></add-card>
+</div>
+
+  
+</template>
+
+<script>
+import addCard from '@/components/AddCard.vue';
+
+export default {
+
+    components: {
+        addCard
+    }
+}
+</script>
+
+<style>
+
+</style>
