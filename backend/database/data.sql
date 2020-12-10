d887307f0527d1001ba31d3296bd15e4503c3183
Nicholas Simmons <midnite87@gmail.com>
2020-12-10 11:03:38 -0500

    Added a test hash for the 'Test' user in the db.  Password is 'test'. Recreate db and rerun data.sql file when you pull code.

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -1,10 +1,16 @@
 BEGIN TRANSACTION;
 
-INSERT INTO public.users (user_id, username, password_hash, role) VALUES (1, 'Nick', 'hjkfdlasbhfdjska', 'ROLE_USER');
+INSERT INTO public.users (user_id, username, password_hash, role) VALUES (1, 'Test', '$2a$10$T.dutnoA.zFw5NBEM0NaC.y6kwrwLcYEisyZl/E3pcZ4mzDsb1k62', 'ROLE_USER');
 
 INSERT INTO public.cards (card_id, question, answer, tags, user_id) VALUES (1, 'What is 1+1', '2', 'Math', 1);
 INSERT INTO public.cards (card_id, question, answer, tags, user_id) VALUES (2, 'What is 1-1', '0', 'Math', 1);
 INSERT INTO public.cards (card_id, question, answer, tags, user_id) VALUES (3, 'water', 'H2O', 'Science', 1);
+
+INSERT INTO public.decks (deck_id, username, title) VALUES (1, 'Test', 'mathTestDeck');
+
+INSERT INTO public.card_deck (deck_id, card_id) VALUES (1, 1);
+INSERT INTO public.card_deck (deck_id, card_id) VALUES (1, 2);
+
 
 SELECT pg_catalog.setval('public.cards_card_id_seq', 3, true);
 
