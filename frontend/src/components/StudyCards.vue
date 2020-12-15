679433dc3c555a2daf3c3d618be95e4fa4eda359
Nicholas Simmons <midnite87@gmail.com>
2020-12-14 21:46:45 -0500

    Added study session scoring.

---
+++ frontend/src/components/StudyCards.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="card-details-container" v-show="!answered">
+    <div class="card-face" v-show="faceUp">
+      <h2>{{ thisCard.question }}</h2>
+      <b-button class="flip-button" v-on:click.prevent="flipCard"
+        >Show me the answer</b-button
+      >
+    </div>
+    <div class="card-face" v-show="!faceUp">
+      <h2>{{ thisCard.answer }}</h2>
+      <div class="study-buttons">
+        <b-button v-on:click.prevent="toggleRight">I got it right!</b-button>
+        <b-button v-on:click.prevent="toggleWrong">I need to study :( </b-button>
+      </div>
+      <b-button class="flip-button" v-on:click.prevent="flipCard"
+        >Show Question</b-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      faceUp: true,
+      answered: false
+    };
+  },
+
+  props: ["thisCard"],
+
+  
+  methods: {
+    flipCard() {
+      this.faceUp = !this.faceUp;
+    },
+
+    toggleRight() {
+      this.answered = true;
+      this.$emit('clicked-right');
+    },
+
+    toggleWrong() {
+      this.answered = true;
+      this.$emit('clicked-wrong', this.thisCard.cardId)
+    }
+  },
+
+
+};
+
+</script>
+
+<style scoped>
+
+* {
+  box-sizing: border-box;
+}
+
+h2 {
+  padding: 10%;
+  font-size: 150%;
+  text-align: center;
+}
+
+.card-face {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2em 1em 0 1em;
+  border-radius: 5px;
+  border: solid #95b0b6 5px;
+  justify-content: center;
+}
+
+.flip-button {
+  width: 70%;
+  margin: 1em 0 1em 0;
+  background-color: #569fad;
+  border: 1px solid transparent;
+  padding: 7px;
+  color: #324b50;
+}
+
+#delete-button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#actual-delete-card-btn {
+  text-align: center;
+  margin: 1em 0 0 0;
+  background-color: #569fad;
+  border: 1px solid transparent;
+  padding: 7px;
+  color: #324b50;
+  width: 50%;
+}
+</style>
--- frontend/src/views/Study.vue
+++ frontend/src/views/Study.vue
@@ -1,48 +1,64 @@
 <template>
   <div id="card-container-card-view">
     <div v-for="card in cardsInCurrentDeck" v-bind:key="card.cardId">
-        <card-details v-bind:this-card="card" />
+      <study-cards  v-on:clicked-wrong="onClickWrong(card.cardId)" 
+                    v-on:clicked-right="onClickRight"
+                    v-bind:this-card="card" />        
+    </div>
+    <div v-show="scoreCard">
+      You got {{cardsInCurrentDeck.length - cardsToReview.length}} right out of {{cardsInCurrentDeck.length}}
     </div>
   </div>
 </template>
 
 <script>
-import DeckServices from '@/services/DeckServices.js';
-import CardDetails from '../components/CardDetails.vue';
+import DeckServices from "@/services/DeckServices.js";
+import StudyCards from "../components/StudyCards.vue";
 
 export default {
-  components: { CardDetails },
+  components: { StudyCards },
 
-data() {
+  data() {
     return {
+      scoreCard: false,
       currentDeckId: this.$route.params.deckId,
-      cardsInCurrentDeck: []
+      cardsInCurrentDeck: [],
+      cardsToReview: [],
+      cardsAnswered: 0
     };
   },
 
-  
-   created() {
-    DeckServices.getDeck(parseInt(this.currentDeckId)).then(
-      (response) => {
-        if (response.status === 200) {
-            this.currentDeck = response.data;
-          for (let card of this.$store.state.cards) {
-            if (this.currentDeck.allCardIds.includes(card.cardId)) {
-              this.cardsInCurrentDeck.push(card);
-              
-            }
+  methods: {
+    onClickWrong (cardId) {
+        this.cardsAnswered++
+        this.cardsToReview.push(cardId);
+        if (this.cardsAnswered === this.cardsInCurrentDeck.length) {
+            this.scoreCard = true;
+        }
+      },
 
+       onClickRight () {
+        this.cardsAnswered++
+        if (this.cardsAnswered === this.cardsInCurrentDeck.length) {
+            this.scoreCard = true;
+        }
+      }
+  },
+
+  created() {
+    DeckServices.getDeck(parseInt(this.currentDeckId)).then((response) => {
+      if (response.status === 200) {
+        this.currentDeck = response.data;
+        for (let card of this.$store.state.cards) {
+          if (this.currentDeck.allCardIds.includes(card.cardId)) {
+            this.cardsInCurrentDeck.push(card);
           }
         }
-        
       }
-    );
+    });
   },
-}
-
- 
+};
 </script>
 
 <style>
-
 </style>
